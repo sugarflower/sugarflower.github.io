0910bf4179d25e6b7c3df0f9851b0ea9756e848b
sugarflower <sse6611@gmail.com>
2018-02-13 02:46:56 +0900

    audio enabled

--- test.js
+++ test.js
@@ -5,6 +5,9 @@ var idx = [ 0,1,0,2 ];
 
 var pos = 0;
 var vec = 1;
+var ere;
+
+var audio;
 
 function setup(){
 	var canvas = createCanvas(720,200);
@@ -13,6 +16,10 @@ function setup(){
 	noSmooth();
 	cnt = 0;
 	img = loadImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAQCAMAAABncAyDAAAAS1BMVEUAAAD///8AAADBhsitb6SGZm4A/30A//8Agv8AAP95AP/PAP//ANeWPBjHXgP/fQDLmkWWPBhhAAD/94LD/4KC/4KC/76C//+Cw/9eGZ2/AAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfiAgwNHSEckxPgAAAAnUlEQVQoz42SCw6EMAhEB+b+d94K1PKpyWJsMg3zBAQAqCR1HfgvVu6Tra+lA27aDcobYOiVbc92TEDTDH/cdMAAwr+WDR1QNKDek4iocAKGhkk5hg7o2qcgFsoroGrs+s6UKqDrMNBL/QLkpp/6vC3RDrAxNZ0YjP9mBFaezUliN3ak3TJiwb3aQyBnG+lz3Aiei7SyKMvNeLP24wdqKQSpXyK6PQAAAABJRU5ErkJggg==");
+
+	audio = new Audio("./lib/audio/Untitled3.mp3");
+	audio.loop = true;
+	audio.play();
 }
 
 function draw(){
